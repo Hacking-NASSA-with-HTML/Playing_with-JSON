aed5c1106b0208759cc4122d409bbea1fda9bd8a
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2021-12-13 21:00:42 +0900

    feat: improve UX

--- script.js
+++ script.js
@@ -24,9 +24,11 @@ clickedButton.onclick = function() {
     if (printedParagraph.innerHTML == 'There\'s no JSON data right now') {
         printedParagraph.innerHTML = extractedJson.name + ', ' + extractedJson.age + ', ' + extractedJson.city;
         clickedButton.innerHTML = 'click me again to cancel JSON adding';
+        printedParagraph.style.color = '#0d941f';
     } else {
         printedParagraph.innerHTML = 'There\'s no JSON data right now';
         clickedButton.innerHTML = 'click me to add JSON data to paragraph';
+        printedParagraph.style.color = '#ff6347';
     }
 };
 
